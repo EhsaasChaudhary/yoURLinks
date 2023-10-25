45c79c1758bfa28592835671cb2ccd0562ee0212
EhsaasChaudhary <21010101028@darshan.ac.in>
2023-10-25 18:10:19 +0530

    table

--- app/URLpage/page.tsx
+++ app/URLpage/page.tsx
@@ -2,6 +2,7 @@ import MyForm from "@/Components/MyForm";
 import { getData } from "@/lib/Getlinks";
 import { Key } from "react";
 
+
 export default async function URLpage() {
   const data = await getData();
 
@@ -30,13 +31,45 @@ export default async function URLpage() {
                   <a href={`${url.orglink}`}></a>
                 </td>
                 <td>
-                <a href="https://yourlinks.vercel.app/{url.orglink}" target="_blank"></a>
+                  <a
+                    href="https://yourlinks.vercel.app/{url.orglink}"
+                    target="_blank"
+                  ></a>
                 </td>
               </tr>
             )
           )}
         </tbody>
       </table>
+
+      {/* <table>
+        <thead>
+          <tr>
+            <th>Original URL</th>
+            <th>Short URL</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(
+            (
+              url: { shortpath: string; orglink: string },
+              index: Key | null | undefined
+            ) => (
+              <tr key={index}>
+                <td>
+                  <a href={`${url.orglink}`}></a>
+                </td>
+                <td>
+                  <a
+                    href="https://yourlinks.vercel.app/{url.orglink}"
+                    target="_blank"
+                  ></a>
+                </td>
+              </tr>
+            )
+          )}
+        </tbody>
+      </table> */}
     </>
   );
 }
