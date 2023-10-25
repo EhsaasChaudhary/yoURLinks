d582e5178fd79a236a57dc2bfa3bf55a1a561b23
EhsaasChaudhary <21010101028@darshan.ac.in>
2023-10-25 23:47:56 +0530

    a

--- app/URLpage/page.tsx
+++ app/URLpage/page.tsx
@@ -5,6 +5,8 @@ import { getData } from "@/lib/Getlinks";
 import { Key } from "react";
 import Link from "next/link";
 
+// Opt out of caching for all data requests in the route segment
+export const dynamic = 'force-dynamic'
 export default async function URLpage() {
   const data = await getData();
 
